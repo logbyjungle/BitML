a9fe47b6a245bce31157f0123cb1f3423be5ecab
Jungle_log <90323544+logbyjungle@users.noreply.github.com>
2025-06-27 12:45:08 +0200

    Update bitml.h

--- bitml.h
+++ bitml.h
@@ -3,106 +3,44 @@
 #include <random>
 #include <cmath>
 #include <cassert>
+#include <numeric>
 
 
-bool ftob(const float& f) {
-    if (f >= 0.5) return true;
-    else return false;
-}
-
-float sum(const std::vector<float>& vec) {
-    float sum = 0.0f;
-    for (const auto& val : vec) {
-        sum += val;
-    }
-    return sum;
-}
-
-
-class Layer {
-    std::vector<uint8_t> bitbiases; // 0 to 255
-    std::vector<bool> boolbiases; // either 0 or 1
-
-    std::vector<std::vector<uint8_t>> bitweights;
-    std::vector<std::vector<bool>> boolweights;
-
-    std::vector<std::vector<uint8_t>> bestbitweights;
-    std::vector<uint8_t> bestbitbiases;
-
-    int size;
-    int ninputs;
-
-public:
-    Layer(const size_t& n_neurons, const size_t& n_inputs) {
-        size = n_neurons;
-        ninputs = n_inputs;
-
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<int> dist(0, 255);
-
-        bitweights.resize(n_neurons);
-        boolweights.resize(n_neurons);
-
-        for (size_t i = 0; i < n_neurons; ++i) {
-            bitbiases.push_back(dist(gen));
-            boolbiases.push_back(ftob(bitbiases[i] / 255.0f));
-            for (size_t j = 0; j < n_inputs; ++j) {
-                bitweights[i].push_back(dist(gen));
-                boolweights[i].push_back(ftob(bitweights[i][j] / 255.0f));
-            }
-        }
+namespace bitnn {
+    bool uint8_t_to_bool(const uint8_t& val) {
+        return val >= 128;
     }
 
-    void update() {
-        for (int i = 0; i < size; i++) {
-            boolbiases[i] = ftob(bitbiases[i] / 255.0f);
-            for (int j = 0; j < ninputs; j++) {
-                boolweights[i][j] = ftob(bitweights[i][j] / 255.0f);
-            }
-        }
-    }
-
-
-
-    std::vector<int> forward(const std::vector<bool>& inputs) {
-        assert(static_cast<int>(inputs.size()) == ninputs && "Input size mismatch!");
-        std::vector<int> outputs;
-        outputs.reserve(size);
-
-        for (int i = 0; i < size; ++i) {
-            int sum = 0;
-            for (int j = 0; j < ninputs; ++j) {
-                if (boolweights[i][j]) {
-                    sum += static_cast<int>(inputs[j]);
-                }
-            }
-            sum += boolbiases[i];
-            outputs.push_back(sum);
-        }
-        return outputs;
-    }
-
-    static std::vector<bool> activation(const std::vector<int>& outputs) {
-        std::vector<bool> ac_outputs(outputs.size(), false);
+    // default activation function provided by the bitnn module, can be replaced
+    std::vector<bool> default_activation(const std::vector<int>& outputs) {
+        std::vector<bool> activated;
         for (size_t i = 0; i < outputs.size(); i++) {
-            if (outputs[i] >= 1) {
-                ac_outputs[i] = true;
+            if (outputs[i]) {
+                activated.push_back(true);
+            }
+            else {
+                activated.push_back(false);
             }
         }
-        return ac_outputs;
+        return activated;
     }
 
-    static std::vector<float> softmax(const std::vector<int>& outputs) {
+    // used to select which activation function to use
+    std::vector<bool> activation(const std::vector<int>& values, std::vector<bool>(*func)(const std::vector<int>&) = default_activation) {
+        return func(values);
+    }
+
+    std::vector<float> softmax(std::vector<int> outputs) {
         std::vector<float> smaxvec;
         std::vector<float> expvec;
 
-        int maxval = *std::max_element(outputs.begin(), outputs.end()); // subtract max to prevent overflow
+        int maxval = *std::max_element(outputs.begin(), outputs.end());
         for (const auto& val : outputs) {
-            expvec.push_back(std::exp(static_cast<float>(val - maxval))); // use float to be safe
+            expvec.push_back(std::exp(static_cast<float>(val - maxval)));
         }
 
-        float sumexp = sum(expvec);
+        float sumexp = std::accumulate(expvec.begin(), expvec.end(), 0.0f);
+
         for (const auto& val : expvec) {
             smaxvec.push_back(val / sumexp);
         }
@@ -110,7 +48,7 @@ public:
         return smaxvec;
     }
 
-    static float getloss(std::vector<float> smaxedin, std::vector<bool> rightout) {
+    float getloss(const std::vector<float>& smaxedin, const std::vector<bool>& rightout) {
         constexpr float epsilon = 1e-7f;
         float loss = 0.0f;
 
@@ -126,81 +64,144 @@ public:
         return loss;
     }
 
-    void newbestparams() {
-        bestbitbiases = bitbiases;
-        bestbitweights = bitweights;
-    }
 
-    void oldparams() {
-        bitbiases = bestbitbiases;
-        bitweights = bestbitweights;
-    }
+    class Layer {
+    private:
+        std::vector<std::vector<bool>> boolweights;
+        std::vector<std::vector<uint8_t>> bitweights;
+        std::vector<std::vector<uint8_t>> bestbitweights;
+        // every neuron contains a vector of weigths with len == inputs
+        const int ninputs;
+        const int nneurons;
+    public:
+        Layer(const int& n_inputs, const int& n_neurons)
+            : ninputs(n_inputs), nneurons(n_neurons)
+        {
+            bitweights.resize(n_neurons);
+            boolweights.resize(n_neurons);
 
-    void tweak(const int& learningrate) {
-        static std::random_device rd;
-        static std::mt19937 gen(rd());
-        std::uniform_int_distribution<int> dist(0, 1);
-
-        for (uint8_t& bias : bitbiases) {
-            int sign = dist(gen) == 0 ? -1 : 1;
-            int new_val = bias + learningrate * sign;
-
-            if (new_val < 0) new_val = 0;
-            if (new_val > 255) new_val = 255;
-
-            bias = static_cast<uint8_t>(new_val);
+            for (int i = 0; i < n_neurons; i++) {
+                // assign it random value
+                bitweights[i].resize(n_inputs, 128);
+                boolweights[i].resize(n_inputs, true);
+            }
+            bestbitweights = bitweights;
         }
 
-        for (std::vector<uint8_t>& vecweights : bitweights) {
-            for (uint8_t& weight : vecweights) {
-                int sign = dist(gen) == 0 ? -1 : 1;
-                int new_val = weight + learningrate * sign;
+        // takes the inputs and gives an output that will have to be put in the activation function
+        std::vector<int> forward(const std::vector<bool>& inputs, const bool& firstlayer = false) {
+            assert(static_cast<int>(inputs.size()) == ninputs && "Input size mismatch!");
+            std::vector<int> outputs;
+            outputs.reserve(nneurons);
+            int sum;
+            for (int i = 0; i < nneurons; i++) {
+                sum = 0;
+                for (int j = 0; j < ninputs; j++) {
+                    if (boolweights[i][j] && (inputs[j] || firstlayer)) {
+                        sum++;
+                    }
+                }
+                outputs.push_back(sum);
+            }
+            return outputs;
+        }
 
-                if (new_val < 0) new_val = 0;
-                if (new_val > 255) new_val = 255;
-
-                weight = static_cast<uint8_t>(new_val);
+        void update() {
+            for (int i = 0; i < nneurons; i++) {
+                for (int j = 0; j < ninputs; j++) {
+                    if (bitweights[i][j] >= 128) {
+                        boolweights[i][j] = true;
+                    }
+                    else {
+                        boolweights[i][j] = false;
+                    }
+                }
             }
         }
-    }
 
-};
-
-class NN {
-    std::vector<Layer> layers;
-
-public:
-    NN(std::vector<int> neurons, const int& inputs) {
-        neurons.insert(neurons.begin(), inputs);
-        for (size_t i = 1; i < neurons.size(); i++) {
-            layers.push_back(Layer(neurons[i], neurons[i - 1]));
+        void newbestparams() {
+            bestbitweights = bitweights;
         }
-    }
 
-    void tweak(const int& learningrate) {
-        for (Layer& layer : layers) {
-            layer.tweak(learningrate);
-            layer.update();
+        void oldparams() {
+            bitweights = bestbitweights;
         }
-    }
 
-    void oldparams() {
-        for (Layer& layer : layers) {
-            layer.oldparams();
-            layer.update();
-        }
-    }
+        // randomly modifies the weights
+        void tweak(const int& learningrate) {
+            static std::random_device rd;
+            static std::mt19937 gen(rd());
+            std::uniform_int_distribution<int> dist(0, 1);
 
-    void newbestparams() {
-        for (Layer& layer : layers) {
-            layer.newbestparams();
-        }
-    }
+            for (std::vector<uint8_t>& vecweights : bitweights) {
+                for (uint8_t& weight : vecweights) {
+                    int sign = dist(gen) == 0 ? -1 : 1;
+                    int new_val = weight + learningrate * sign;
 
-    std::vector<float> execute(std::vector<bool> inputs) {
-        for (size_t i = 0; i < layers.size() - 1; i++) {
-            inputs = Layer::activation(layers[i].forward(inputs));
+                    if (new_val < 0) new_val = 0;
+                    if (new_val > 255) new_val = 255;
+
+                    weight = static_cast<uint8_t>(new_val);
+                }
+            }
         }
-        return Layer::softmax(layers[layers.size() - 1].forward(inputs));
-    }
-};
+    };
+
+    class NN {
+    private:
+        std::vector<bitnn::Layer> layers;
+        const int size;
+    public:
+        NN(const std::vector<int>& neurons)
+            : size(neurons.size() - 1)
+        {
+            for (size_t i = 1; i < neurons.size(); i++) {
+                layers.push_back(Layer(neurons[i - 1], neurons[i]));
+            }
+        }
+
+        // randomly modifies the entire nn or just a single layer
+        void tweak(const int& learningrate, const int& layerindex = -1) {
+            if (layerindex == -1) {
+                for (Layer& layer : layers) {
+                    layer.tweak(learningrate);
+                    layer.update();
+                }
+            }
+            else {
+                layers[layerindex].tweak(learningrate);
+                layers[layerindex].update();
+            }
+        }
+
+        void oldparams() {
+            for (Layer& layer : layers) {
+                layer.oldparams();
+                layer.update();
+            }
+        }
+
+        void newbestparams() {
+            for (Layer& layer : layers) {
+                layer.newbestparams();
+            }
+        }
+
+        std::vector<float> execute(std::vector<bool> inputs) {
+            inputs = bitnn::activation(layers[0].forward(inputs, true));
+            for (size_t i = 1; i < layers.size() - 1; i++) {
+                inputs = bitnn::activation(layers[i].forward(inputs));
+            }
+            return bitnn::softmax(layers[layers.size() - 1].forward(inputs));
+        }
+
+        // gives back the number of layers
+        int size() {
+            return size;
+        }
+
+    };
+
+
+
+}
