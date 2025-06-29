0306e548f98866c4d75e32b89ed73efad65e0c87
Jungle_log <90323544+logbyjungle@users.noreply.github.com>
2025-06-29 19:37:12 +0200

    Update bitml.h

--- bitml.h
+++ bitml.h
@@ -4,7 +4,7 @@
 #include <cmath>
 #include <cassert>
 #include <numeric>
-
+#include <string>
 
 namespace bitnn {
     bool uint8_t_to_bool(const uint8_t& val) {
@@ -49,11 +49,11 @@ namespace bitnn {
     }
 
     float getloss(const std::vector<float>& smaxedin, const std::vector<bool>& rightout) {
-        constexpr float epsilon = 1e-7f;
+        constexpr float EPSILON = 1e-7f;
         float loss = 0.0f;
 
         for (size_t i = 0; i < smaxedin.size(); i++) {
-            float p = std::min(std::max(smaxedin[i], epsilon), 1.0f - epsilon);
+            float p = std::min(std::max(smaxedin[i], EPSILON), 1.0f - EPSILON);
             if (rightout[i]) {
                 loss += -log(p);
             }
@@ -71,11 +71,11 @@ namespace bitnn {
         std::vector<std::vector<uint8_t>> bitweights;
         std::vector<std::vector<uint8_t>> bestbitweights;
         // every neuron contains a vector of weigths with len == inputs
-        const int ninputs;
-        const int nneurons;
+        const int N_INPUTS;
+        const int N_NEURONS;
     public:
         Layer(const int& n_inputs, const int& n_neurons)
-            : ninputs(n_inputs), nneurons(n_neurons)
+            : N_INPUTS(n_inputs), N_NEURONS(n_neurons)
         {
             bitweights.resize(n_neurons);
             boolweights.resize(n_neurons);
@@ -90,13 +90,12 @@ namespace bitnn {
 
         // takes the inputs and gives an output that will have to be put in the activation function
         std::vector<int> forward(const std::vector<bool>& inputs, const bool& firstlayer = false) {
-            assert(static_cast<int>(inputs.size()) == ninputs && "Input size mismatch!");
-            std::vector<int> outputs;
-            outputs.reserve(nneurons);
+            assert(static_cast<int>(inputs.size()) == N_INPUTS && "Input size mismatch!");
+            std::vector<int> outputs(N_NEURONS);
             int sum;
-            for (int i = 0; i < nneurons; i++) {
+            for (int i = 0; i < N_NEURONS; i++) {
                 sum = 0;
-                for (int j = 0; j < ninputs; j++) {
+                for (int j = 0; j < N_INPUTS; j++) {
                     if (boolweights[i][j] && (inputs[j] || firstlayer)) {
                         sum++;
                     }
@@ -106,9 +105,13 @@ namespace bitnn {
             return outputs;
         }
 
+        int neuron_n_weight(const int& index) {
+            return boolweights[index].size();
+        }
+
         void update() {
-            for (int i = 0; i < nneurons; i++) {
-                for (int j = 0; j < ninputs; j++) {
+            for (int i = 0; i < N_NEURONS; i++) {
+                for (int j = 0; j < N_INPUTS; j++) {
                     if (bitweights[i][j] >= 128) {
                         boolweights[i][j] = true;
                     }
@@ -145,15 +148,45 @@ namespace bitnn {
                 }
             }
         }
+
+        // gives back the bestbitweights in boolean form
+        std::vector<std::vector<bool>> getweights() {
+            std::vector<std::vector<bool>> vec;
+            vec.resize(bestbitweights.size());
+            for (size_t i = 0; i < bestbitweights.size(); i++) {
+                vec[i].resize(bestbitweights[i].size());
+                for (size_t j = 0; j < bestbitweights[i].size(); j++) {
+                    vec[i].push_back(uint8_t_to_bool(bestbitweights[i][j]));
+                }
+            }
+            return vec;
+        }
+
+        // load boolean weights
+        void load(const std::vector<std::vector<bool>>& bool_weights) {
+            boolweights = bool_weights;
+            for (int i = 0; i < N_NEURONS; i++) {
+                for (int j = 0; j < N_INPUTS; j++) {
+                    bitweights[i][j] = 128 * bool_weights[i][j];
+                }
+            }
+            bestbitweights = bitweights;
+        }
+
+        int size() {
+            return N_NEURONS;
+        }
+
+
     };
 
     class NN {
     private:
         std::vector<bitnn::Layer> layers;
-        const int size;
+        const int N_LAYERS;
     public:
         NN(const std::vector<int>& neurons)
-            : size(neurons.size() - 1)
+            : N_LAYERS(neurons.size() - 1)
         {
             for (size_t i = 1; i < neurons.size(); i++) {
                 layers.push_back(Layer(neurons[i - 1], neurons[i]));
@@ -197,11 +230,132 @@ namespace bitnn {
 
         // gives back the number of layers
         int size() {
-            return size;
+            return N_LAYERS;
         }
+
+        Layer getlayer(const int& index) {
+            return layers[index];
+        }
+
+        // saves the boolean weights of the model, for execution only
+        void save(const std::string& filename) {
+            std::vector<std::vector<std::vector<bool>>> vec;
+            vec.reserve(N_LAYERS);
+            for (int i = 0; i < N_LAYERS; i++) {
+                vec.push_back(layers[i].getweights());
+            }
+
+            std::ofstream out(filename);
+            std::string metadata = "BitNNboolmodel1";
+            out << metadata << "\n";
+
+            out << vec.size() << "\n";
+            for (const auto& layer : vec) {
+                out << layer.size() << "\n";
+                for (const auto& row : layer) {
+                    out << row.size() << " ";
+                    for (bool b : row) {
+                        out << (b ? 1 : 0) << " ";
+                    }
+                    out << "\n";
+                }
+            }
+        }
+
+
+        void load(const std::string& filename) {
+            std::ifstream in(filename);
+            assert(in && "Failed to open file for reading");
+
+            std::string metadata;
+            std::getline(in, metadata);
+
+            if (metadata == "BitNNboolmodel1") {
+                size_t dim1;
+                in >> dim1;
+                // checks if it has the right amount of layers
+                assert((static_cast<int>(dim1) == N_LAYERS) && "model shape is incorrect: layers");
+                std::vector<std::vector<std::vector<bool>>> vec(dim1);
+                for (size_t i = 0; i < dim1; ++i) {
+                    size_t dim2;
+                    in >> dim2;
+                    // checks if every layer has the right amount of neurons
+                    assert((static_cast<int>(dim2) == getlayer(i).size()) && "model shape is incorrect: neurons");
+                    vec[i].resize(dim2);
+
+                    for (size_t j = 0; j < dim2; ++j) {
+                        size_t dim3;
+                        in >> dim3;
+                        // checks if every neuron has the right amount of weights
+                        assert((static_cast<int>(dim3) == getlayer(i).neuron_n_weight(j)) && "model shape is incorrect: weights");
+                        vec[i][j].resize(dim3);
+
+                        for (size_t k = 0; k < dim3; ++k) {
+                            int bit;
+                            in >> bit;
+                            vec[i][j][k] = (bit != 0);
+                        }
+                    }
+                }
+
+                for (int i = 0; i < N_LAYERS; i++) {
+                    layers[i].load(vec[i]);
+                }
+            }
+            else {
+                std::cout << "failed to load model " << filename << "\n";
+            }
+        }
+
+
+
 
     };
 
+    class Optimizer {
+    private:
+        const int EPOCHS;
+        NN& network;
+        const std::vector<std::vector<bool>> YS;
+        const std::vector<std::vector<bool>> XS;
+        const int DEBUGMODE;
 
+    public:
+
+
+        Optimizer(NN& neuralnetwork, const int& epochs, const std::vector<std::vector<bool>>& Xs, const std::vector<std::vector<bool>>& Ys, const int& debugmode = 0)
+            : EPOCHS(epochs), network(neuralnetwork), YS(Ys), XS(Xs), DEBUGMODE(debugmode) {
+            assert((YS.size() == XS.size()) && "Ys and Xs have different size"); // also assert if Xs[0].size() == layer0_n_inputs .. and if Ys[0].size() == layerlast_n_neurons
+        }
+
+        void randomsearch(const int& learningrate) {
+            float lowestloss = 999999999.9f;
+            std::cout << "starting training...\n";
+            for (int epoch = 0; epoch < EPOCHS; epoch++) {
+                float totalloss = 0;
+                for (size_t i = 0; i < XS.size(); i++) {
+                    auto out = network.execute(XS[i]);
+                    float loss = bitnn::getloss(out, YS[i]);
+                    totalloss += loss;
+                }
+
+                if (totalloss < lowestloss) {
+                    lowestloss = totalloss;
+                    network.newbestparams();
+                }
+                else {
+                    network.oldparams();
+                }
+
+                if (DEBUGMODE > 0) {
+                    std::cout << "epoch: " << epoch << " lowestloss: " << lowestloss / XS.size() << "\n";
+                }
+                network.tweak(learningrate);
+
+
+            }
+        }
+
+    };
 
 }
