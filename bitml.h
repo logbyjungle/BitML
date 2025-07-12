a8b38b25b19dc369ea5d5799fd315227174a9ac8
Jungle_log <90323544+logbyjungle@users.noreply.github.com>
2025-07-12 12:57:56 +0200

    new activation function

--- bitml.h
+++ bitml.h
@@ -39,7 +39,7 @@ class Randomness{
             call = 64;
             val = next();
         }
-        call--;
+        --call;
         return (val & call) != 0;
     }
 
@@ -67,7 +67,7 @@ public:
     void set_weights(const std::bitset<N>& new_weights) {
         weights = new_weights;
         bestweights = weights;
-        for (size_t i = 0; i < N; i++) {
+        for (size_t i = 0; i < N; ++i) {
             if (weights[i]) {
                 bitweights[i] = 128;
             } else {
@@ -83,7 +83,7 @@ public:
 
     // this function is really slow
     void tweak(const uint8_t& learningrate = 1) {
-        for (size_t i = 0; i < N; i++) {
+        for (size_t i = 0; i < N; ++i) {
             if (Randomness::next_bool()) {
                 if (bitweights[i] + learningrate <= 255) {
                     bitweights[i] += static_cast<uint8_t>(learningrate);
@@ -125,20 +125,12 @@ class Layer {
 private:
     std::array<bitnn::Neuron<N>, M> neurons;
 
-    static std::bitset<M> default_activation(const std::array<int, M> &inputs) {
-        std::bitset<M> outputs;
-        for (size_t i = 0; i < M; ++i) {
-            outputs.set(i,inputs[i] != 0);
-        }
-        return outputs;
-    }
-
 public:
     Layer() {};
 
     std::array<int, M> forward(const std::bitset<N> &inputs) const {
         std::array<int, M> outputs;
-        for (size_t i = 0; i < M; i++) {
+        for (size_t i = 0; i < M; ++i) {
             outputs[i] = neurons[i].result(inputs);
         }
         return outputs;
@@ -166,23 +158,37 @@ public:
         }
     }
 
-    std::bitset<M> activation(const std::array<int, M> &inputs, std::bitset<M> (*activation_func)(const std::array<int, M> &) = default_activation) const {
-        return activation_func(inputs);
+    template <int Mod = 1, int Param = 0 >
+    std::bitset<M> activation(const std::array<int, M> &inputs) const {
+        std::bitset<M> outputs;
+        static_assert(1 <= Mod && Mod <= 2 && "activation function type hasnt been found");
+        // outputs if it has received inputs
+        if constexpr (Mod == 1) {
+            static_assert(Param == 0 && "activation function parameter is not 0 even tho a function that doesnt need a parameter has been used");
+            for (size_t i = 0; i < M; ++i) {
+                outputs.set(i,inputs[i] != 0);
+            }
+        }
+        // outputs if Mod% inputs are positive
+        else if constexpr (Mod == 2) {
+            for (size_t i = 0; i < M; ++i) {
+                outputs.set(i,inputs[i] >= static_cast<int>((N/100)*Param));
+            }
+        }
+        return outputs;
     }
 
     static std::array<float, M> softmax(const std::array<int, M> &inputs) {
-        std::array<float, M> smaxarr;
         std::array<float, M> exparr;
-        int maxval = *std::max_element(inputs.begin(), inputs.end());
 
-        for (size_t i = 0; i < M; i++) {
-            exparr[i] = std::exp(static_cast<float>(inputs[i] - maxval));
+        for (size_t i = 0; i < M; ++i) {
+            exparr[i] = std::exp(static_cast<float>(inputs[i] - *std::max_element(inputs.begin(), inputs.end())));
         }
 
-        float sumexp = std::accumulate(exparr.begin(), exparr.end(), 0.0f);
+        std::array<float, M> smaxarr;
 
-        for (size_t i = 0; i < M; i++) {
-            smaxarr[i] = exparr[i] / sumexp;
+        for (size_t i = 0; i < M; ++i) {
+            smaxarr[i] = exparr[i] / std::accumulate(exparr.begin(), exparr.end(), 0.0f);
         }
         return smaxarr;
     }
@@ -190,12 +196,11 @@ public:
     static float getloss(const std::array<float, M> &smaxedin, const std::bitset<M> &rightout) {
         static constexpr float EPSILON = 1e-7f;
         float loss = 0.0f;
-        for (size_t i = 0; i < M; i++) {
-            float p = std::min(std::max(smaxedin[i], EPSILON), 1.0f - EPSILON);
+        for (size_t i = 0; i < M; ++i) {
             if (rightout[i]) {
-                loss += -logf(p);
+                loss += -logf(std::min(std::max(smaxedin[i], EPSILON), 1.0f - EPSILON));
             } else {
-                loss += -logf(1.0f - p);
+                loss += -logf(1.0f -std::min(std::max(smaxedin[i], EPSILON), 1.0f - EPSILON));
             }
         }
         return loss;
@@ -217,6 +222,7 @@ private:
     template <size_t I = 0>
     void construct_layers() {
         if constexpr (I < SIZE - 1) {
+            // maybe use unique ptr instead of raw pointer
             voidarr[I] = new Layer<get<I>(), get<I+1>()>;
             construct_layers<I + 1>();
         }
@@ -256,10 +262,8 @@ public:
         if constexpr (I < SIZE - 1) { // for layer in NN
             auto* layer = static_cast<Layer<get<I>(), get<I+1>()>*>(voidarr[I]);
             for (size_t i = 0; i < get<I+1>(); ++i) { // for neuron in layer
-                auto& neuron = (*layer).get_neuron(i);
                 size_t num_bytes = (get<I>() + 7) / 8;
                 std::vector<uint8_t> bits(num_bytes);
-
                 file.read(reinterpret_cast<char*>(bits.data()), static_cast<std::streamsize>(num_bytes));
                 std::bitset<get<I>()> loaded_weights;
                 for (size_t j = 0; j < get<I>(); ++j) { // for weight in neuron
@@ -267,6 +271,7 @@ public:
                         loaded_weights.set(j);
                     }
                 }
+                auto& neuron = (*layer).get_neuron(i);
                 neuron.set_weights(loaded_weights);
 
             }
@@ -279,12 +284,12 @@ public:
         if constexpr (I < SIZE - 1) { // for layer in NN
 
             auto* layer = static_cast<Layer<get<I>(),get<I+1>()>*>(voidarr[I]);
-            for (size_t i = 0; i < get<I+1>(); i++) { // for neuron in layer
+            for (size_t i = 0; i < get<I+1>(); ++i) { // for neuron in layer
                 const auto& neuron = (*layer).get_neuron(i);
-                const auto& weights = neuron.get_weights();
                 size_t num_bytes = (get<I>() + 7) / 8;
                 std::vector<uint8_t> bits(num_bytes, 0);
-                for (size_t j = 0; j < get<I>(); j++) { // for inputweights in neuron
+                const auto& weights = neuron.get_weights();
+                for (size_t j = 0; j < get<I>(); ++j) { // for inputweights in neuron
                     if (weights[j]) {
                         bits[j / 8] |= (1 << (j % 8));
                     }
@@ -301,7 +306,7 @@ public:
         if (filename.empty() || std::filesystem::exists(filename)) {
             int number = 0;
             do {
-                number++;
+                ++number;
                 filename = "model_" + std::to_string(number) + ".blmod";
             }
             while (std::filesystem::exists(filename));
@@ -315,7 +320,7 @@ public:
         // saving just the bitset values and not the traning ones
         if (mode == 0) {
             save_0(file);
-            std::cout << "saved model (no training values) to file " << filename << "\n";
+            std::cout << "saved model (no training values) to file " << filename << '\n';;
         }
 
         file.close();
@@ -323,13 +328,13 @@ public:
 
     void load(const std::string& filename, const int& mode = 0) {
         if (! std::filesystem::exists(filename)) {
-            std::cout << "unable to find file " << filename << "\n";
+            std::cout << "unable to find file " << filename << '\n';
             return;
         }
         std::ifstream file(filename, std::ios::binary);
         if (mode == 0) {
             load_0(file);
-            std::cout << "loaded model (no training values) to file " << filename << "\n";
+            std::cout << "loaded model (no training values) to file " << filename << '\n';
         }
         file.close();
     }
@@ -342,17 +347,15 @@ public:
         return *this;
     }
 
-    template <size_t I = 1>
+    template <size_t I = 1, int Mod = 1, int Param = 1>
     std::array<float, VALUES[SIZE - 1]> execute(std::bitset<VALUES[I-1]> inputs) {
-
         auto outputs = (*static_cast<Layer<VALUES[I - 1], VALUES[I]> *>(voidarr[I - 1])).forward(inputs);
 
         if constexpr (I == SIZE-1) {
             return Layer<VALUES[SIZE-2],VALUES[SIZE-1]>::softmax(outputs);
         } else {
-
-            auto activated = (*static_cast<Layer<VALUES[I - 1], VALUES[I]> *>(voidarr[I - 1])).activation(outputs);
-            return execute<I+1>(activated);
+            auto activated = (*static_cast<Layer<VALUES[I - 1], VALUES[I]> *>(voidarr[I - 1])).template activation<Mod,Param>(outputs);
+            return execute<I+1,Mod,Param>(activated);
         }
 
     }
@@ -406,17 +409,18 @@ class Optimizer {
     : net(network), EPOCHS(epochs), XS(Xs), YS(Ys),DEBUGMODE(debugmode) {
     }
 
+    template <int Mod = 1, int Param = 0>
     void randomsearch(const uint8_t& learningrate = 1) {
         float lowestloss = 99999999.9f;
         if  (DEBUGMODE >= 1) {
             std::cout << "starting training...\n";
         }
 
-        for (int epoch = 1; epoch <= EPOCHS; epoch++) {
+        for (int epoch = 1; epoch <= EPOCHS; ++epoch) {
             float totalloss = 0.0f;
 
-            for (size_t i = 0; i < L ; i++) {
-                auto out = net.execute(XS[i]);
+            for (size_t i = 0; i < L ; ++i) {
+                auto out = net.template execute<1,Mod,Param>(XS[i]);
                 float loss = Layer<PRELAST,LAST>::getloss(out, YS[i]);
                 totalloss += loss;
             }
@@ -426,11 +430,11 @@ class Optimizer {
                 net.newbestparams();
                 if (DEBUGMODE >= 2) {
                     std::cout << "new lowest loss:\n";
-                    std::cout << "epoch: " << epoch << "  loss: " << totalloss/static_cast<float>(XS.size()) << "  lowestloss: " << lowestloss/static_cast<float>(XS.size()) << "\n";
+                    std::cout << "epoch: " << epoch << "  loss: " << totalloss/static_cast<float>(XS.size()) << "  lowestloss: " << lowestloss/static_cast<float>(XS.size()) << '\n';
                 }
             } else {
                 if (DEBUGMODE >= 3) {
-                    std::cout << "epoch: " << epoch << "  loss: " << totalloss/static_cast<float>(XS.size()) << "  lowestloss: " << lowestloss/static_cast<float>(XS.size()) << "\n";
+                    std::cout << "epoch: " << epoch << "  loss: " << totalloss/static_cast<float>(XS.size()) << "  lowestloss: " << lowestloss/static_cast<float>(XS.size()) << '\n';
                 }
                 net.oldparams();
             }
